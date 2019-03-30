a01225fd8498a0b5c63d215a73747a400ae2192d
Shahrose <shahrosetheme@gmail.com>
2019-03-30 13:45:31 +0500

    Created the UI

---
+++ Assets/MainMenuAssets/Scripts/MainMenuController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+//Put script on Canvas
+public class MainMenuController : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    private Resolution[] _resolutions;
+    [SerializeField]
+    private Dropdown _resolutionDropdown;
+
+    public void Start()
+    {
+        resolutionFinder();
+    }
+
+    //For Main Menu
+
+    public void playGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+    //For Option Menu
+
+    public void volumeChanger(float volume)
+    {
+        Debug.Log(volume);
+        audioMixer.SetFloat("VolumeGame", volume);
+    }
+
+    public void setQuality(int qualityIndex) {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void setFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void setResolution(int resolutionIndex)
+    {
+        Resolution newResolution = _resolutions[resolutionIndex];
+        Screen.SetResolution(newResolution.width, newResolution.height, Screen.fullScreen);
+    }
+
+    private void resolutionFinder()
+    {
+        _resolutions = Screen.resolutions;
+        _resolutionDropdown.ClearOptions();
+
+        int currentResolution = 0;
+
+        List<string> converted = new List<string>();
+        for (int i = 0; i < _resolutions.Length; i++)
+        {
+            converted.Add(_resolutions[i].width + " X " + _resolutions[i].height);
+
+            if (_resolutions[i].width == Screen.currentResolution.width && _resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolution = i;
+            }
+        }
+
+        //Did the above loop because Dropdown only accepts lists of type 
+        _resolutionDropdown.AddOptions(converted);
+        _resolutionDropdown.value = currentResolution;
+        _resolutionDropdown.RefreshShownValue();
+    }
+
+
+}
---
+++ Assets/MainMenuAssets/Scripts/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _pauseButton;
+    [SerializeField]
+    private GameObject _pausedPanel;
+
+    public void pauseGame()
+    {
+        _pauseButton.SetActive(false);
+        Time.timeScale = 0f;
+        _pausedPanel.SetActive(true);
+    }
+
+    public void resumeGame()
+    {
+        _pauseButton.SetActive(true);
+        Time.timeScale = 1f;
+        _pausedPanel.SetActive(false);
+    }
+
+    public void loadMenu() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
